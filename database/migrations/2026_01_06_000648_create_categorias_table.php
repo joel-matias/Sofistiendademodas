8753613fdf955447afc44aca0f3e74efc417315b
joel12-Sant <joelsantiagos000@gmail.com>
2026-01-05 18:09:34 -0600

    se agrego migracion de categorias

--- database/migrations/2026_01_06_000648_create_categorias_table.php
+++ database/migrations/2026_01_06_000648_create_categorias_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre')->unique();
+            $table->string('slug')->unique();
+            $table->text('descripcion')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('categorias');
